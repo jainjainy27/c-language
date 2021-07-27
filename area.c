e056e84889202a9b76bc780c47fb331540baa105
jainy jain <84911198+jainjainy27@users.noreply.github.com>
2021-07-27 21:12:48 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int length,breath;
+    printf("what is length?");
+    scanf("%d",&length);
+    printf("what is breath?");
+    scanf("%d",&breath);
+    printf("the area of rectangk=le is %d",length*breath);
+    return 0;
+}
